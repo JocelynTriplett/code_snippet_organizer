8fbd07dcc2766a3a1e555411b99df0a4fb0d6cc5
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-09-05 21:08:30 -0400

    add lorem ipsum for snippet body

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ const expressValidator = require('express-validator');
 const User = user_model.User;
 const Snippet = snippet_model.Snippet;
 const moment = require('moment');
+const loremIpsum = require('lorem-ipsum');
 
 const app = express();
 
@@ -106,9 +107,16 @@ app.post('/new/', function (req, res) {
     {title: req.body.title,
     user: res.locals.user.username,
     language: req.body.language,
-    body: req.body.body,
+    body: loremIpsum({
+    count: 2,                      // Number of words, sentences, or paragraphs to generate.
+    units: 'sentences' ,           // Generate words, sentences, or paragraphs.
+    sentenceLowerBound: 5 ,        // Minimum words per sentence.
+    sentenceUpperBound: 15 ,       // Maximum words per sentence.
+    format: 'plain' ,              // Plain text or html
+    random: Math.random ,          // A PRNG function. Uses Math.random by default
+    }),
     date_created: moment()}
-        )
+  )
 
   .then(function (snippet) {
 
